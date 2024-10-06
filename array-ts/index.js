865c65393f266c12e007938f640ff0f466916ed2
farhan maulana <farhanmaulana1710@gmail.com>
2024-10-06 13:35:54 +0700

    history latihan ts ku

---
+++ array-ts/index.js
@@ -0,0 +1,13 @@
+// let angkDong: [number, string, boolean]; //? tuple
+// let angkDong: (number | string | boolean)[]; //? array campuran bebas tidak mengikuti aturan tuple
+// let angkDong: (number | string | boolean)[] = []; //? array campuran bebas tidak mengikuti aturan tuple
+// const angkDong = [1, "lita", true]; //? array campurran tetapi dengan code variable aatau tipe variable nya const yang mana tetap dan nilai harus di inisialisasikan di awal
+// const angkDong: (number | string)[] = ['asep', "lita"]; //? array campuran tetapi dengan code variable aatau tipe variable nya const yang mana tetap dan nilai harus di inisialisasikan di awal
+// angkDong = [1, "lita"];
+// angkDong.push(0);
+// angkDong.push("lita");
+// angkDong.push(true);
+// console.log(angkDong);
+var lita = ["asep", 40];
+lita[0] = "lita";
+console.log(lita);
---
+++ object-ts/index.js
@@ -0,0 +1,6 @@
+var mahasiswa = {
+    nama: "lita",
+    umur: 22,
+    aktif: true,
+};
+console.log({ mahasiswa: mahasiswa });
