ce8bd4607e39c74b79a3152320ed7bffc370aab1
farhan maulana <farhanmaulana1710@gmail.com>
2024-10-07 16:02:07 +0700

    interface-ts

--- object-ts/index.js
+++
@@ -1,6 +0,0 @@
-var mahasiswa = {
-    nama: "lita",
-    umur: 22,
-    aktif: true,
-};
-console.log({ mahasiswa: mahasiswa });
--- union-ts/index.js
+++
@@ -1,3 +0,0 @@
-var angka; //? variable yang di buat dengan esksplisit type union
-angka = 10;
-console.log(angka);
